a308e6200735d2e14c276e1761dc87b828973f41
ariel pena ocando <arielpenaocando@ariels-MacBook-Pro.local>
2020-06-06 11:57:20 +0200

    another commit

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
-
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from pages import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('', views.index, name= 'index'),
+    path('', views.index, name= 'about'),
+    path('admin/', admin.site.urls),
+    path('users/', views.usersList.as_view()),
+    path('matches/', views.matchMakingList.as_view())
 
 ]
