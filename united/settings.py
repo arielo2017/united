ad8b831580d89edd2b218f04815d60514cff9a0c
ariel pena ocando <arielpenaocando@ariels-MacBook-Pro.local>
2020-06-06 22:56:58 +0200

    Implement database connection

--- united/settings.py
+++ united/settings.py
@@ -11,9 +11,16 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# This is new:
+dotenv_file = os.path.join(BASE_DIR, "venv")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 
 # Quick-start development settings - unsuitable for production
