38912c3c288315324fd46e0a7aba73db46dd26d8
ariel pena ocando <arielpenaocando@ariels-MacBook-Pro.local>
2020-06-08 15:32:43 +0200

    last adjustments

--- pages/models.py
+++ pages/models.py
@@ -13,7 +13,7 @@ class users(models.Model):
 
 class matchmaking(models.Model):
     user_id = models.ManyToManyField('users')
-    matchname = models.CharField(max_length = 20)
+    matchname = models.CharField(max_length=20)
     datetoplay = models.DateTimeField()
     city = models.CharField(max_length=20)
     price = models.IntegerField()
@@ -25,7 +25,7 @@ class matchmaking(models.Model):
 class training(models.Model):
      
     user_id = models.ManyToManyField('users')
-    form_name = models.CharField(max_length = 20)
+    form_name = models.CharField(max_length=20)
     city = models.CharField(max_length=20)
     date = models.DateTimeField()
     training_id = models.IntegerField()
